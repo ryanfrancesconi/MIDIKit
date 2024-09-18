a5f7bdfe555065761e545c5698afb8dca5f7c0f6
Ryan Francesconi <ryan@add.app>
2024-09-18 08:16:36 -0700

    test remove-disfavoredOverload

--- Package.swift
+++ Package.swift
@@ -47,7 +47,7 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/orchetect/TimecodeKit", from: "2.0.11"),
+        .package(url: "https://github.com/ryanfrancesconi/TimecodeKit", branch: "remove-disfavoredOverload"),
         
         // testing only:
         .package(url: "https://github.com/orchetect/XCTestUtils", from: "1.0.3")
